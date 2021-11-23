05b9030716a1c69528b148ebb3353c046d490a38
Pretty9 <206367405@qq.com>
2021-11-23 15:04:57 +0800

    customize

---
+++ .env.pgsql
@@ -0,0 +1,3 @@
+POSTGRES_PASSWORD=<Databse_Password>
+POSTGRES_USER=<Databse_User>
+POSTGRES_DB=<Databse_Name>
--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -16,9 +16,18 @@ class StatusesIndex < Chewy::Index
         language: 'possessive_english',
       },
     },
+    char_filter: {
+      tsconvert: {
+        type: 'stconvert',
+        keep_both: false,
+        delimiter: '#',
+        convert_type: 't2s',
+      },
+    },
     analyzer: {
       content: {
-        tokenizer: 'uax_url_email',
+        # tokenizer: 'uax_url_email',
+        tokenizer: 'ik_max_word',
         filter: %w(
           english_possessive_stemmer
           lowercase
